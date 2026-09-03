76256a9f9fd9576248f2d1bae640c42b43fe1462
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-09-03 13:51:22 +0530

    ui: load Layersentry navigation compatibility styles

--- ui/src/main.js
+++ ui/src/main.js
@@ -27,6 +27,7 @@ import extensions from './core/ext'
 import './permission' // permission control
 import './utils/filter' // global filter
 import './style/layersentry.less'
+import './style/layersentry-navigation.less'
 import {
   pollJobPlugin,
   notifierPlugin,
